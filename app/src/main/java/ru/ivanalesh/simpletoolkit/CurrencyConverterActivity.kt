fcd643b0286e9cf60edf8dde20cb9db93485cb05
IVaN20 AleshEchkin <ivan20alesh@outlook.com>
2025-02-18 11:47:06 +0400

    update code

--- app/src/main/java/ru/ivanalesh/simpletoolkit/CurrencyConverterActivity.kt
+++ app/src/main/java/ru/ivanalesh/simpletoolkit/CurrencyConverterActivity.kt
@@ -20,12 +20,13 @@ import androidx.core.view.WindowInsetsCompat
 import ru.ivanalesh.simpletweaker.R
 import java.util.*
 import java.text.SimpleDateFormat
+import java.time.LocalDate
 
 class CurrencyConverterActivity : AppCompatActivity() {
 
     private lateinit var timeZoneSpinner : Spinner
     private lateinit var handler: Handler
-    private lateinit var runnable: Runnable
+    private lateinit var runnable1: Runnable
     private var selectedTimeZone = "GMT"
     private lateinit var digitalClockTextView : TextView
     private val timeZoneKey = "selected_time_zone"
@@ -42,53 +43,68 @@ class CurrencyConverterActivity : AppCompatActivity() {
         val backBtn1 = findViewById<ImageButton>(R.id.back_btn_1)
         //var digitalClockTextView = findViewById<TextView>(R.id.digitalClock)
         digitalClockTextView = findViewById(R.id.digitalClock)
-        timeZoneSpinner = findViewById(R.id.timeZoneSpinner)
+        //timeZoneSpinner = findViewById(R.id.timeZoneSpinner)
         backBtn1.setOnClickListener { goBack() }
         val timeZones = TimeZone.getAvailableIDs().toList()
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, timeZones)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        val sharedPreferences = getSharedPreferences("AppPreferences", Context.MODE_PRIVATE)
-        selectedTimeZone = sharedPreferences.getString(timeZoneKey, "GMT") ?: "GMT"
+        //val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, getFormattedTimeZones())
+        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        //val sharedPreferences = getSharedPreferences("AppPreferences", Context.MODE_PRIVATE)
+        selectedTimeZone = TimeZone.getDefault().toString()//sharedPreferences.getString(timeZoneKey, "GMT") ?: "GMT"
+        //timeZoneSpinner.adapter = adapter
 
-        val timeZoneIndex = timeZones.indexOf(selectedTimeZone)
-        if (timeZoneIndex != -1){
-            timeZoneSpinner.setSelection(timeZoneIndex)
-        }
-        timeZoneSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
-                selectedTimeZone = parent.getItemAtPosition(position) as String
-                sharedPreferences.edit().putString(timeZoneKey, selectedTimeZone).apply()
-            }
+        //val timeZoneIndex = timeZones.indexOf(selectedTimeZone)
+        //if (timeZoneIndex != -1){
+         //   timeZoneSpinner.setSelection(timeZoneIndex)
+        //}
+        //timeZoneSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+        //    override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+        //        selectedTimeZone = parent.getItemAtPosition(position) as String
+         //       sharedPreferences.edit().putString(timeZoneKey, selectedTimeZone).apply()
+         //   }
 
-            override fun onNothingSelected(parent: AdapterView<*>) {
+          //  override fun onNothingSelected(parent: AdapterView<*>) {
 
-            }
-        }
-        //handler = Handler(Looper.getMainLooper())
-        runnable = object : Runnable {
+         //   }
+        //}
+        handler = Handler(Looper.getMainLooper())
+        runnable1 = object : Runnable {
             override fun run() {
                 updateTime()
                 digitalClockTextView.postDelayed(this, 1000)
             }
         }
-        digitalClockTextView.postDelayed(runnable, 1000)
+        digitalClockTextView.post(runnable1)
     }
     //@RequiresApi(Build.VERSION_CODES.N)
     private fun updateTime() {
-        val timeZone = TimeZone.getTimeZone(selectedTimeZone)
+        /*val timeZone = TimeZone.getTimeZone(selectedTimeZone)
         //val timeZone2 = java.util.TimeZone.getTimeZone(selectedTimeZone)
         val calendar = Calendar.getInstance()
         calendar.timeZone = timeZone
         val timeFormat = SimpleDateFormat("HH:mm:ss", Locale.getDefault())
-        timeFormat.timeZone = timeZone
-        digitalClockTextView.text = timeFormat.format(calendar.time)
+        timeFormat.timeZone = timeZone*/
+        val time = Calendar.getInstance().time
+        val formatter = SimpleDateFormat("HH:mm:ss")
+        val current = formatter.format(time)
+        digitalClockTextView.text = current
     }
     private fun goBack(){
         finish()
     }
+    /*private fun getFormattedTimeZones(): List<String> {
+        return TimeZone.getAvailableIDs()
+            .map { id ->
+                val timeZone = TimeZone.getTimeZone(id)
+                val offsetHours = timeZone.rawOffset / 1000 / 60 / 60
+                val offsetFormatted = if (offsetHours >= 0) "+$offsetHours" else "$offsetHours"
+                "$id (GMT$offsetFormatted)" to offsetHours
+            }
+            .sortedBy { it.second } // Сортировка по смещению
+            .map { it.first }
+    }*/
     override fun onDestroy() {
         super.onDestroy()
         // Остановка обновления времени при закрытии приложения
-        handler.removeCallbacks(runnable)
+        handler.removeCallbacks(runnable1)
     }
 }
